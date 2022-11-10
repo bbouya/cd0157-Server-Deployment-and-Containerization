3dc1764341d7fd7f92aa0253806639a7f8e50f1c
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-11-10 08:55:56 +0100

    files

---
+++ trust.py
@@ -0,0 +1,14 @@
+# files trust.json old version added by me
+
+{{
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": "arn:aws:iam::519002666132:root"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }},
